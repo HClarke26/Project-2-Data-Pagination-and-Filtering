8a2d4218a90a9745db80d90f99b82554c4a4cc2c
HClarke26 <harryclarke26@gmail.com>
2022-06-08 13:25:36 +0100

    file update

--- js/script.js
+++ js/script.js
@@ -52,10 +52,10 @@ function addPagination(list) {
          linkList.insertAdjacentHTML("beforeend", button);
       }
 
-   // Giving the first pagination button a class of "active"
+   // giveing the first pagination button a class of "active"
    document.querySelector(".link-list button").className = "active";
 
-   // Listening for click and showing corresponding page
+   // listening for click and showing corresponding page
    linkList.addEventListener('click',(e) => {
       
       if (e.target.tagName === "BUTTON") {
@@ -66,63 +66,6 @@ function addPagination(list) {
    });
  };
 
-<<<<<<< HEAD:js/script.js
-
-// Function to create serach bar
-function addSearchBar() {
-   // Selecting header class
-   const selectHeader = document.querySelector(".header");
-   // Creating search bar
-   const searchBar = `
-         <label for="search" class="student-search">
-            <span>Search by name</span>
-            <input id="search" placeholder="Search by name...">
-            <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-         </label>
-       `;
-   // Inserting search bar to the DOM.
-   selectHeader.insertAdjacentHTML("beforeend", searchBar);
-}
-
-
-errorMessage.className = 'errorMessage';
-searchBar.parentNode.appendChild(errorMessage);
-
-
-//searchFilter function is used to find a student by first or last name, errorMessage will be returned if there are no matches
-/*function filterStudents(userInput, studentsList){
-   const search = document.getElementById("search"); 
-   const errorMessage = document.createElement('p'); 
-   
-   let filteredResults = [];
-   errorMessage.textContent = '';
-   for(let i = 0 ; i < studentsList.length; i++){
-      if(studentsList[i].name.first.toLowerCase().includes(userInput.toLowerCase()) || 
-      studentsList[i].name.last.toLowerCase().includes(userInput.toLowerCase()) ){
-         filteredResults.push(studentsList[i]);
-         errorMessage.style.display="none";
-      } else if (filteredResults.length == 0) { 
-         errorMessage.textContent = '" ' + userInput +' " '+ ' is not found, please try again.';
-         errorMessage.style.display="block";
-      }
-   }
-   showPage(filteredResults,1);
-   addPagination(filteredResults);
-}*/
-
-
-
-/*let searchName= "";
-const inputSearch = document.querySelector("#search");
-inputSearch.addEventListener("keyup", (e) => {
-   searchName = e.target.value;
-   searchFilter(searchName, data);
- }); */
-
-
-=======
->>>>>>> dc1f26af7fc90f18e03bf215e7a2d1c847598417:data-pagination-and-filtering-v1/js/script.js
 // Calling functions
 showPage(data, 1);
 addPagination(data);
-
